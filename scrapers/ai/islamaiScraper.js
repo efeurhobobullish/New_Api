6ca95fdb57c867023adb9819f23f3154f8aab0b1
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-06-27 10:14:52 +0100

    Add files via upload

---
+++ scrapers/ai/islamaiScraper.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+
+async function islamai(question) {
+    const url = 'https://vercel-server-psi-ten.vercel.app/chat';
+    const data = {
+        text: question,
+        array: [
+            {
+                content: "Assalamualaikum",
+                role: "user"
+            },
+            {
+                content: "Waalaikumsalam",
+                role: "assistant"
+            }
+        ]
+    };
+
+    try {
+        const response = await axios.post(url, data, {
+            headers: {
+                'Content-Type': 'application/json',
+                'User-Agent': 'Mozilla/5.0 (Android 10; Mobile; rv:131.0) Gecko/131.0 Firefox/131.0',
+                'Referer': 'https://islamandai.com/'
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error sending request:', error);
+        throw error;
+    }
+}
+
+module.exports = { islamai };
