49ddbea2f6ccaab20f9f58adbe889465abd5857f
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2026-03-24 16:59:14 +0100

    Update InstagramScraper.js

--- scrapers/downloader/InstagramScraper.js
+++ scrapers/downloader/InstagramScraper.js
@@ -1,49 +1,56 @@
-const axios = require("axios");
-const cheerio = require("cheerio");
-const qs = require("qs");
+const cheerio = require('cheerio');
+const axios = require('axios');
 
-async function instagram(url) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      // Step 1: Get token
-      const { data } = await axios.get(
-        "https://www.instagramsave.com/download-instagram-videos.php",
-        {
-          headers: {
-            "user-agent":
-              "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-            "cookie":
-              "PHPSESSID=ugpgvu6fgc4592jh7ht9d18v49; _ga=GA1.2.1126798330.1625045680; _gid=GA1.2.1475525047.1625045680;",
-          },
+async function igdl(Link) {
+    return new Promise(async (resolve, reject) => {
+        const Form = {
+            url: Link,
+            submit: ""
         }
-      );
-
-      const $ = cheerio.load(data);
-      const token = $("#token").attr("value");
-
-      // Step 2: Post to get download links
-      const response = await axios.post(
-        "https://www.instagramsave.com/system/action.php",
-        qs.stringify({
-          url: url,
-          action: "post",
-          token: token,
-        }),
-        {
-          headers: {
-            "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-            "user-agent":
-              "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-            "referer": "https://www.instagramsave.com/download-instagram-videos.php",
-          },
+        
+        try {
+            const res = await axios(`https://downloadgram.org/`, {
+                method: "POST",
+                data: new URLSearchParams(Object.entries(Form)),
+                headers: {
+                    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                    "accept-language": "en-US,en;q=0.9,id;q=0.8",
+                    "cache-control": "max-age=0",
+                    "content-type": "application/x-www-form-urlencoded",
+                    "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"90\", \"Google Chrome\";v=\"90\"",
+                    "cookie": "_ga=GA1.2.1695343126.1621491858; _gid=GA1.2.28178724.1621491859; __gads=ID=8f9d3ef930e9a07b-2258e672bec80081:T=1621491859:RT=1621491859:S=ALNI_MbqLxhztDiYZttJFX2SkvYei6uGOw; __atuvc=3%7C20; __atuvs=60a6eb107a17dd75000; __atssc=google%3B2; _gat_gtag_UA_142480840_1=1"
+                },
+                referrerPolicy: "strict-origin-when-cross-origin"
+            })
+            
+            const $ = cheerio.load(res.data)
+            let url = $('#downloadBox').find('a').attr('href');
+            
+            const respon = await axios(Link, {
+                method: "GET",
+                data: null,
+                headers: {
+                    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                    "accept-language": "en-US,en;q=0.9,id;q=0.8",
+                    "cache-control": "max-age=0",
+                    "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"90\", \"Google Chrome\";v=\"90\"",
+                    "cookie": "ig_did=08A3C465-7D43-4D8A-806A-88F98384E63B; ig_nrcb=1; mid=X_ipMwALAAFgQ7AftbrkhIDIdXJ8; fbm_124024574287414=base_domain=.instagram.com; shbid=17905; ds_user_id=14221286336; csrftoken=fXHAj5U3mcJihQEyVXfyCzcg46lHx7QD; sessionid=14221286336%3A5n4czHpQ0GRzlq%3A28; shbts=1621491639.7673564; rur=FTW"
+                },
+                referrerPolicy: "strict-origin-when-cross-origin"
+            })
+            
+            const ch = cheerio.load(respon.data)
+            let title = ch('title').text().trim()
+            
+            resolve({
+                link: url,
+                desc: title
+            })
+            
+        } catch (err) {
+            reject(err.message || err)
         }
-      );
-
-      resolve(response.data.medias);
-    } catch (err) {
-      reject(err.message || err);
-    }
-  });
+    })
 }
 
-module.exports = { instagram };
+module.exports = { igdl };
